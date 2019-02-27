41d15b6b09c8284dad2554fe8a683d2c498c9c70
pdelboca <patriciodelboca@gmail.com>
2019-02-27 15:07:09 -0300

    Archivo data.R y corrida de roxygenise.

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Nombre de Personas.
+#'
+#' Nombres registrados en el Registro Nacional de las Personas agrupados por año.
+#' La información surge de los puestos de toma de trámites del DNI en todo el país,
+#' incluyendo argentinos y extranjeros nacionalizados. (1922-2015)
+#'
+#' @format Un data frame con 3 variables: \code{anio}, \code{nombre},
+#'   \code{cantidad}
+"nombres"
